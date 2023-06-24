f7a84d481672de99e76bef144605ffdb2e6142d7
Nathanuel Dixon <nathan0322@hotmail.com>
2023-06-24 01:35:31 -0400

    again

--- server/index.mjs
+++ server/index.mjs
@@ -13,8 +13,8 @@ app.use(cors({
     credentials: true,
     origin: '*'
 }))
-app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
