ca068a6a1436487dc49f1b5af0e80fdd4a21d5f1
Nathanuel Dixon <nathan0322@hotmail.com>
2023-06-24 02:02:11 -0400

    lets see

--- client/src/components/MyRoutes.js
+++ client/src/components/MyRoutes.js
@@ -7,7 +7,8 @@ import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import SignupScreen from '../screens/SignUpScreen';
 import Home from '../screens/Home';
 import Reviews from '../screens/Reviews';
-export const fetchlink = process.env.NODE_ENV === 'production ' ? '' : 'http://localhost:3001'
+export const fetchlink = process.env.NODE_ENV === 'production' ? 'https://eatfinder.onrender.com/' : 'http://localhost:3001'
+console.log("fetchlink: ", fetchlink)
 
 export default function MyRoutes() {
   const {user, setUser, logout} = useContext(AuthContext);
